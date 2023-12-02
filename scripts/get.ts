104cf59ca939b0df65a56e259651a08a01c9ddf2
Balczer Dominik <balczer.dominik@gmail.com>
2023-12-02 11:44:09 +0100

    fix fs

--- scripts/get.ts
+++ scripts/get.ts
@@ -1,11 +1,19 @@
-import { writeFileSync } from "fs";
+import { existsSync, mkdirSync, writeFileSync } from "fs";
 import * as dotenv from "dotenv";
+import path from "path";
 
 dotenv.config();
 
 const year = new Date().getFullYear();
 const day = new Date().getDate();
 
+const tgtDir = path.join(__dirname, `../${year}/day${day}`);
+const fileName = "input.txt";
+
+if (!existsSync(tgtDir)) {
+  mkdirSync(tgtDir, { recursive: true });
+}
+
 fetch(`https://adventofcode.com/${year}/day/${day}/input`, { headers: { Cookie: `session=${process.env.SESSION_COOKIE}` } })
   .then((res) => res.text().then((text) => text.substring(0, text.lastIndexOf("\n"))))
-  .then((text) => writeFileSync(`./${year}/day${day}/input.txt`, text, { flag: "w" }));
+  .then((text) => writeFileSync(path.join(tgtDir, fileName), text, { flag: "w" }));
