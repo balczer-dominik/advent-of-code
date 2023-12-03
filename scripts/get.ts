89cc250468ef467fed5edfe5752993e2909536a2
Balczer Dominik <balczer.dominik@gmail.com>
2023-12-03 12:31:03 +0100

    improve get script

--- scripts/get.ts
+++ scripts/get.ts
@@ -1,4 +1,4 @@
-import { existsSync, mkdirSync, writeFileSync } from "fs";
+import { copyFile, copyFileSync, existsSync, mkdirSync, writeFileSync } from "fs";
 import * as dotenv from "dotenv";
 import path from "path";
 
@@ -7,12 +7,16 @@ dotenv.config();
 const year = new Date().getFullYear();
 const day = new Date().getDate();
 
+const templateSrc = path.join(__dirname, `../template/aoc.ts`);
 const tgtDir = path.join(__dirname, `../${year}/day${day}`);
 const fileName = "input.txt";
 
 if (!existsSync(tgtDir)) {
   mkdirSync(tgtDir, { recursive: true });
 }
+
+copyFileSync(templateSrc, path.join(tgtDir, "aoc.ts"));
+writeFileSync(path.join(tgtDir, "test_input.txt"), "");
 
 fetch(`https://adventofcode.com/${year}/day/${day}/input`, { headers: { Cookie: `session=${process.env.SESSION_COOKIE}` } })
   .then((res) => res.text().then((text) => text.substring(0, text.lastIndexOf("\n"))))
