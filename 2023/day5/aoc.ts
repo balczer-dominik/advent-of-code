4407ab303b21eb696b39d2d4d34a87ac8e377cd8
Balczer Dominik <balczer.dominik@gmail.com>
2023-12-05 23:18:38 +0100

    tidy up

--- 2023/day5/aoc.ts
+++ 2023/day5/aoc.ts
@@ -3,7 +3,7 @@ import { getIntersection, minReduce, simpleParseInt, sortAscEx } from "../../uti
 import { Tuple } from "../../util/Tuple";
 
 const [input, testInput] = readInputs(__dirname);
-const TEST = true;
+const TEST = false;
 
 type Mapping = {
   from: number;
@@ -44,9 +44,9 @@ export const func2 = () =>
       (ranges, map) =>
         ranges.flatMap((range) => {
           const intersections = map
-            .map(({ from, to, offset }) => ({ range: getIntersection(range, [from, to]), offset }))
+            .map(({ from, to, offset }) => ({ range: getIntersection(range, [from, to])!, offset }))
             .filter(({ range }) => range !== null)
-            .sort(sortAscEx(({ range }) => range![0]));
+            .sort(sortAscEx(({ range }) => range[0]));
 
           let nonIntersectingRanges = [];
           if (intersections.isEmpty()) {
@@ -54,22 +54,19 @@ export const func2 = () =>
           } else {
             if (intersections.length >= 2)
               for (let rangeIndex = 0; rangeIndex < intersections.length - 1; rangeIndex++) {
-                const from = intersections[rangeIndex].range![1] + 1;
-                const to = intersections[rangeIndex + 1].range![0] - 1;
+                const from = intersections[rangeIndex].range[1] + 1;
+                const to = intersections[rangeIndex + 1].range[0] - 1;
                 if (to < from) continue;
                 nonIntersectingRanges.push([from, to]);
               }
 
-            if (intersections[0].range![0] !== range[0])
-              nonIntersectingRanges = [[range[0], intersections[0].range![0] - 1], ...nonIntersectingRanges];
-            if (intersections.last()!.range![1] !== range[1])
-              nonIntersectingRanges = [...nonIntersectingRanges, [intersections.last()!.range![1] + 1, range[1]]];
+            if (intersections[0].range[0] !== range[0])
+              nonIntersectingRanges = [[range[0], intersections[0].range[0] - 1], ...nonIntersectingRanges];
+            if (intersections.last()!.range[1] !== range[1])
+              nonIntersectingRanges = [...nonIntersectingRanges, [intersections.last()!.range[1] + 1, range[1]]];
           }
 
-          return [
-            ...nonIntersectingRanges,
-            ...intersections.map(({ range, offset }) => [range![0] + offset, range![1] + offset]),
-          ] as Tuple[];
+          return [...nonIntersectingRanges, ...intersections.map(({ range, offset }) => [range[0] + offset, range[1] + offset])] as Tuple[];
         }),
       seeds.partition(2).map(([start, size]) => [start, start + size - 1]) as Tuple[]
     )
