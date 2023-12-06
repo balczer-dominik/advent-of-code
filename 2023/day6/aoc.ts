1735f71950efca070f57844ab896c9d996d3aed6
Balczer Dominik <balczer.dominik@gmail.com>
2023-12-06 09:08:25 +0100

    tidy up

--- 2023/day6/aoc.ts
+++ 2023/day6/aoc.ts
@@ -17,10 +17,7 @@ const [times, distances] = raw.map((row) =>
 export const func1 = () => times.map((time, i) => getWinningPossibilities(time, distances[i])).reduce(multiplyNumbers);
 export const func2 = () => getWinningPossibilities(parseInt(times.join("")), parseInt(distances.join("")));
 
-const getWinningPossibilities = (time: number, distance: number) => {
-  return (
-    _.rangeRight(0, time).find((charge) => charge * (time - charge) > distance)! -
-    _.range(0, time).find((charge) => charge * (time - charge) > distance)! +
-    1
-  );
-};
+const getWinningPossibilities = (time: number, distance: number) =>
+  _.rangeRight(0, time).find((charge) => charge * (time - charge) > distance)! -
+  _.range(0, time).find((charge) => charge * (time - charge) > distance)! +
+  1;
