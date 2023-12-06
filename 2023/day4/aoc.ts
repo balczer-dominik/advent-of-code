2610025f6e75c278f0ce89db81017f4c57824f4a
Balczer Dominik <balczer.dominik@gmail.com>
2023-12-06 09:00:13 +0100

    tidy up

--- 2023/day4/aoc.ts
+++ 2023/day4/aoc.ts
@@ -1,44 +1,32 @@
 import { readInputs } from "../../util/input";
-import "../../util/helpers";
 import { simpleParseInt, sumNumbers } from "../../util/helpers";
 
 const [input, testInput] = readInputs(__dirname);
 const TEST = false;
 
-type Card = {
-  cardId: number;
-  winningNumbers: number[];
-  myNumbers: number[];
-};
+const raw = TEST ? testInput : input;
 
-const parseInput = (): Card[] => {
-  const raw = TEST ? testInput : input;
+const cards = raw.map((row) => {
+  const [cardIdRaw, numbersRaw] = row.split(":");
+  const [winningNumbersRaw, myNumbersRaw] = numbersRaw.split("|");
+  return {
+    cardId: parseInt(cardIdRaw.split("Card")[1].trim()),
+    winningNumbers: winningNumbersRaw
+      .split(" ")
+      .filter((x) => x !== "")
+      .map(simpleParseInt),
+    myNumbers: myNumbersRaw
+      .split(" ")
+      .filter((x) => x !== "")
+      .map(simpleParseInt),
+  };
+});
 
-  return raw.map((row) => {
-    const [cardIdRaw, numbersRaw] = row.split(":");
-    const cardId = cardIdRaw.split("Card")[1].trim();
-    const [winningNumbersRaw, myNumbersRaw] = numbersRaw.split("|");
-    return {
-      cardId: parseInt(cardId),
-      winningNumbers: winningNumbersRaw
-        .split(" ")
-        .filter((x) => x !== "")
-        .map(simpleParseInt),
-      myNumbers: myNumbersRaw
-        .split(" ")
-        .filter((x) => x !== "")
-        .map(simpleParseInt),
-    };
-  });
-};
-const cards = parseInput();
-
-export const func1 = () => {
-  return cards.reduce((acc, card) => {
+export const func1 = () =>
+  cards.reduce((acc, card) => {
     const value = card.myNumbers.filter((number) => card.winningNumbers.includes(number)).length;
     return acc + (value === 0 ? 0 : Math.pow(2, value - 1));
   }, 0);
-};
 
 export const func2 = () => {
   const cardValues = cards.reduce(
