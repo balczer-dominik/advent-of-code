9406de6dc640a7a9b5825eacf5394653fc93c923
Balczer Dominik <balczer.dominik@gmail.com>
2023-12-09 11:38:30 +0100

    tidy up

--- 2023/day9/aoc.ts
+++ 2023/day9/aoc.ts
@@ -8,15 +8,15 @@ const raw = TEST ? testInput : input;
 
 const valueHistories = raw.map((row) => row.numberSequence());
 
-const predict = (rear: boolean, reducer: (a: number, b: number) => number) =>
+const predict = (trailExtractor: (seq: number[]) => number, reducer: (a: number, b: number) => number) =>
   valueHistories.sum((values) => {
     const trail: number[] = [];
     while (values.some((value) => value !== 0)) {
-      trail.push(rear ? values.last()! : values[0]);
+      trail.push(trailExtractor(values));
       values = values.slice(0, -1).map((value, i) => values[i + 1] - value);
     }
     return trail.reduceRight(reducer);
   });
 
-export const func1 = () => predict(true, sumNumbers);
-export const func2 = () => predict(false, sortDesc);
+export const func1 = () => predict((seq) => seq.last()!, sumNumbers);
+export const func2 = () => predict((seq) => seq[0], sortDesc);
