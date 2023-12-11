e631e8a4dca9a96202dca100ec3f437c0d0980d7
Balczer Dominik <balczer.dominik@gmail.com>
2023-12-11 18:21:46 +0100

    tidy up helper file

--- util/helpers.ts
+++ util/helpers.ts
@@ -1,97 +1,41 @@
 import _ from "lodash";
 
-export type ObjectValues<T> = T[keyof T];
+// ------------STRING------------
 
-export const reverseKeyValues = (obj: object) => Object.fromEntries(Object.entries(obj).map(([k, v]) => [v, k]));
+export const simpleParseInt = (number: string) => parseInt(number);
 
-export const uniqueFilter = <T>(value: T, index: number, self: T[]) => {
-  return self.indexOf(value) === index;
-};
-
-export const uniqueByFilter =
-  <T, E>(keyExtractor: (obj: T) => E) =>
-  (value: T, index: number, self: T[]) =>
-    self.findIndex((s) => keyExtractor(s) === keyExtractor(value))! === index;
-
-export const rangesIntersect = ([fromA, toA]: [number, number], [fromB, toB]: [number, number]) => !(toA < fromB || fromA > toB);
-
-export const getIntersection = ([fromA, toA]: [number, number], [fromB, toB]: [number, number]) => {
-  const fromResult = Math.max(fromA, fromB);
-  const toResult = Math.min(toA, toB);
-  return fromResult > toResult ? null : [fromResult, toResult];
-};
-
-export const bfs = <Field>(from: Field, to: Field, neighborExtractor: (field: Field) => Field[]) => {
-  const queue: Field[][] = [];
-  const visited: Field[] = [from];
-  let curr: Field[] = [from];
-
-  while (curr.last() !== to) {
-    if (!queue.isEmpty()) {
-      curr = queue.shift()!;
-    }
-
-    const head = curr.last()!;
-
-    neighborExtractor(head).forEach((neighbor) => {
-      if (!visited.some((n) => _.isEqual(n, neighbor))) {
-        visited.push(neighbor);
-        queue.push([...curr, neighbor]);
-      }
-    });
+declare global {
+  interface String {
+    isNumber: () => boolean;
+    toNumber: () => number;
+    numberSequence: () => number[];
   }
+}
 
-  return curr.filter((_, i) => i !== 0);
+String.prototype.isNumber = function () {
+  return !isNaN(parseInt(this.toString()));
+};
+String.prototype.toNumber = function () {
+  return parseInt(this.valueOf());
+};
+String.prototype.numberSequence = function () {
+  return this.split(" ")
+    .filter((s) => s !== "" && s.isNumber())
+    .map(simpleParseInt);
 };
 
-export const sortAsc = (a: number, b: number) => {
-  return a - b;
-};
-
-export const sortAscEx =
-  <T>(extractor: (item: T) => number) =>
-  (a: T, b: T) =>
-    extractor(a) - extractor(b);
-
-export const sortDesc = (a: number, b: number) => {
-  return b - a;
-};
-
-export const sortDescEx =
-  <T>(extractor: (item: T) => number) =>
-  (a: T, b: T) =>
-    extractor(b) - extractor(a);
+// ------------NUMBER------------
 
 export const sumNumbers = (a: number, b: number) => a + b;
 export const subtractNumbers = (a: number, b: number) => a - b;
 export const multiplyNumbers = (a: number, b: number) => a * b;
 export const divideNumbers = (a: number, b: number) => a / b;
-export const maxReduce = (acc: number, curr: number) => (acc > curr ? acc : curr);
-export const minReduce = (acc: number, curr: number) => (acc < curr ? acc : curr);
 
 declare global {
   interface Number {
     isBetween: (lowerBound: number, upperBound: number, inclusive?: boolean) => boolean;
     gcd: (otherNumber: number) => number;
     lcm: (otherNumber: number) => number;
-  }
-  interface Array<T> {
-    last: () => T | undefined;
-    isEmpty: () => boolean;
-    partition: (groupLength: number) => Array<Array<T>>;
-    sum: (mapper?: (el: T, idx: number) => number) => number;
-    product: (mapper?: (el: T, idx: number) => number) => number;
-    lcm: (mapper?: (el: T, idx: number) => number) => number;
-    gcd: (mapper?: (el: T, idx: number) => number) => number;
-    min: (mapper?: (el: T, idx: number) => number) => number;
-    max: (mapper?: (el: T, idx: number) => number) => number;
-    distinct: (mapper?: (el: T) => any) => Array<T>;
-    toObject: <U>(keyMapper: (el: T, idx: number) => string | number, valueMapper: (el: T, idx: number) => U) => { [key: string]: U };
-  }
-  interface String {
-    isNumber: () => boolean;
-    toNumber: () => number;
-    numberSequence: () => number[];
   }
 }
 
@@ -110,6 +54,49 @@ Number.prototype.gcd = function (otherNumber: number) {
 Number.prototype.lcm = function (otherNumber: number) {
   return (this.valueOf() * otherNumber) / this.gcd(otherNumber);
 };
+
+// ------------OBJECT------------
+
+export type ObjectValues<T> = T[keyof T];
+export const reverseKeyValues = (obj: object) => Object.fromEntries(Object.entries(obj).map(([k, v]) => [v, k]));
+
+// ------------ARRAY------------
+
+export const uniqueFilter = <T>(value: T, index: number, self: T[]) => self.indexOf(value) === index;
+export const uniqueByFilter =
+  <T, E>(keyExtractor: (obj: T) => E) =>
+  (value: T, index: number, self: T[]) =>
+    self.findIndex((s) => keyExtractor(s) === keyExtractor(value))! === index;
+
+export const sortAsc = (a: number, b: number) => a - b;
+export const sortDesc = (a: number, b: number) => b - a;
+export const sortAscEx =
+  <T>(extractor: (item: T) => number) =>
+  (a: T, b: T) =>
+    extractor(a) - extractor(b);
+export const sortDescEx =
+  <T>(extractor: (item: T) => number) =>
+  (a: T, b: T) =>
+    extractor(b) - extractor(a);
+
+export const maxReduce = (acc: number, curr: number) => (acc > curr ? acc : curr);
+export const minReduce = (acc: number, curr: number) => (acc < curr ? acc : curr);
+
+declare global {
+  interface Array<T> {
+    last: () => T | undefined;
+    isEmpty: () => boolean;
+    partition: (groupLength: number) => Array<Array<T>>;
+    sum: (mapper?: (el: T, idx: number) => number) => number;
+    product: (mapper?: (el: T, idx: number) => number) => number;
+    lcm: (mapper?: (el: T, idx: number) => number) => number;
+    gcd: (mapper?: (el: T, idx: number) => number) => number;
+    min: (mapper?: (el: T, idx: number) => number) => number;
+    max: (mapper?: (el: T, idx: number) => number) => number;
+    distinct: (mapper?: (el: T) => any) => Array<T>;
+    toObject: <U>(keyMapper: (el: T, idx: number) => string | number, valueMapper: (el: T, idx: number) => U) => { [key: string]: U };
+  }
+}
 
 Array.prototype.last = function <T>(): T | undefined {
   return this[this.length - 1];
@@ -149,16 +136,36 @@ Array.prototype.toObject = function <T, U>(keyMapper: (el: T, idx: number) => st
   return this.reduce((obj, _, i) => ({ ...obj, [keys[i]]: values[i] }), {});
 };
 
-String.prototype.isNumber = function (): boolean {
-  return !isNaN(parseInt(this.toString()));
-};
-String.prototype.toNumber = function () {
-  return parseInt(this.valueOf());
-};
-String.prototype.numberSequence = function () {
-  return this.split(" ")
-    .filter((s) => s !== "" && s.isNumber())
-    .map(simpleParseInt);
+// ------------TUPLE------------
+
+export const rangesIntersect = ([fromA, toA]: [number, number], [fromB, toB]: [number, number]) => !(toA < fromB || fromA > toB);
+export const getIntersection = ([fromA, toA]: [number, number], [fromB, toB]: [number, number]) => {
+  const fromResult = Math.max(fromA, fromB);
+  const toResult = Math.min(toA, toB);
+  return fromResult > toResult ? null : [fromResult, toResult];
 };
 
-export const simpleParseInt = (number: string) => parseInt(number);
+// ------------GRAPH------------
+
+export const bfs = <Field>(from: Field, to: Field, neighborExtractor: (field: Field) => Field[]) => {
+  const queue: Field[][] = [];
+  const visited: Field[] = [from];
+  let curr: Field[] = [from];
+
+  while (curr.last() !== to) {
+    if (!queue.isEmpty()) {
+      curr = queue.shift()!;
+    }
+
+    const head = curr.last()!;
+
+    neighborExtractor(head).forEach((neighbor) => {
+      if (!visited.some((n) => _.isEqual(n, neighbor))) {
+        visited.push(neighbor);
+        queue.push([...curr, neighbor]);
+      }
+    });
+  }
+
+  return curr.slice(1);
+};
