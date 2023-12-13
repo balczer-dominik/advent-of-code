45da8cd9b74a38eec604776d45d8a35372492d3a
Balczer Dominik <balczer.dominik@gmail.com>
2023-12-13 12:46:40 +0100

    refactor

--- 2023/day13/aoc.ts
+++ 2023/day13/aoc.ts
@@ -1,107 +1,39 @@
 import { readInputs } from "../../util/input";
 import "../../util/helpers";
 
-//26602
-
 const [input, testInput] = readInputs(__dirname);
-const TEST = true;
+const TEST = false;
 const raw = TEST ? testInput : input;
 
-const patterns = raw.reduce(
-  (acc: string[][], row) => {
-    if (row[0] === undefined) acc.push([]);
-    else acc.last()!.push(row);
-    return acc;
-  },
-  [[]]
-);
+const patterns: string[][] = [[]];
+raw.forEach((row) => {
+  if (row[0] === undefined) patterns.push([]);
+  else patterns.last()!.push(row);
+});
 
-//30472
-
-export const func1 = () => {
-  return patterns.sum((pattern, idx) => {
-    const transposed = pattern.transposeStringArray();
-
-    horizontalLoop: for (let i = 0; i < pattern.length - 1; i++) {
-      const refSize = Math.min(i + 1, pattern.length - i - 1);
-      // console.log(`i: ${i} | refSize: ${refSize}`);
-      for (let j = 0; j < refSize; j++) {
-        if (pattern[i + j + 1] !== pattern[i - j]) continue horizontalLoop;
+const getReflectionDistance = (pattern: string[], multiplier: number = 1, canRepair: boolean = false) => {
+  loop: for (let reflectionLine = 0; reflectionLine < pattern.length - 1; reflectionLine++) {
+    const refSize = Math.min(reflectionLine + 1, pattern.length - reflectionLine - 1);
+    let repaired = !canRepair;
+    for (let distanceFromReflection = 0; distanceFromReflection < refSize; distanceFromReflection++) {
+      if (pattern[reflectionLine + distanceFromReflection + 1] !== pattern[reflectionLine - distanceFromReflection]) {
+        if (repaired) continue loop;
+        const toRepair = pattern[reflectionLine + distanceFromReflection + 1]
+          .split("")
+          .filter((c, cIdx) => c !== pattern[reflectionLine - distanceFromReflection][cIdx]).length;
+        if (toRepair > 1) continue loop;
+        repaired = true;
       }
-
-      console.log(idx + 1, "horizontal", i, (i + 1) * 100);
-      return (i + 1) * 100;
     }
 
-    verticalLoop: for (let i = 0; i < transposed.length - 1; i++) {
-      const refSize = Math.min(i + 1, transposed.length - i - 1);
-      // console.log(`i: ${i} | refSize: ${refSize}`);
-      for (let j = 0; j < refSize; j++) {
-        if (transposed[i + j + 1] !== transposed[i - j]) continue verticalLoop;
-      }
-
-      console.log(idx + 1, "vertical", i, i + 1);
-      return i + 1;
-    }
-
-    console.log("panic, no match");
-
-    return 0;
-  });
+    if (repaired) return (reflectionLine + 1) * multiplier;
+  }
 };
 
-export const func2 = () => {
-  return patterns.sum((pattern, idx) => {
-    const transposed = pattern.transposeStringArray();
+export const func1 = () =>
+  patterns.sum((pattern) => getReflectionDistance(pattern, 100) || getReflectionDistance(pattern.transposeStringArray()) || 0);
 
-    let repaired = false;
-
-    horizontalLoop: for (let i = 0; i < pattern.length - 1; i++) {
-      const refSize = Math.min(i + 1, pattern.length - i - 1);
-      // console.log(`i: ${i} | refSize: ${refSize}`);
-      for (let j = 0; j < refSize; j++) {
-        if (pattern[i + j + 1] !== pattern[i - j]) {
-          if (repaired) {
-            repaired = false;
-            continue horizontalLoop;
-          }
-          const toRepair = pattern[i + j + 1].split("").filter((c, cIdx) => c !== pattern[i - j][cIdx]).length;
-
-          if (toRepair > 1) continue horizontalLoop;
-          repaired = true;
-        }
-      }
-
-      if (repaired) {
-        console.log(idx + 1, "horizontal", i, (i + 1) * 100);
-        return (i + 1) * 100;
-      }
-    }
-
-    verticalLoop: for (let i = 0; i < transposed.length - 1; i++) {
-      const refSize = Math.min(i + 1, transposed.length - i - 1);
-      // console.log(`i: ${i} | refSize: ${refSize}`);
-      for (let j = 0; j < refSize; j++) {
-        if (transposed[i + j + 1] !== transposed[i - j]) {
-          if (repaired) {
-            repaired = false;
-            continue verticalLoop;
-          }
-          const toRepair = transposed[i + j + 1].split("").filter((c, cIdx) => c !== transposed[i - j][cIdx]).length;
-
-          if (toRepair > 1) continue verticalLoop;
-          repaired = true;
-        }
-      }
-
-      if (repaired) {
-        console.log(idx + 1, "vertical", i, i + 1);
-        return i + 1;
-      }
-    }
-
-    console.log("panic, no match");
-
-    return 0;
-  });
-};
+export const func2 = () =>
+  patterns.sum(
+    (pattern) => getReflectionDistance(pattern, 100, true) || getReflectionDistance(pattern.transposeStringArray(), 1, true) || 0
+  );
