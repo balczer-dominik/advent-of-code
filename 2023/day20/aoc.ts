22ec476b7a918fb784238b54cfc48f0687f7a49d
Balczer Dominik <balczer.dominik@gmail.com>
2023-12-20 20:27:45 +0100

    refactor

--- 2023/day20/aoc.ts
+++ 2023/day20/aoc.ts
@@ -1,184 +1,142 @@
 import { readInputs } from "../../util/input";
 import "../../util/helpers";
+import _ from "lodash";
 
 const [input, testInput] = readInputs(__dirname);
 const TEST = false;
 const raw = TEST ? testInput : input;
 
-type Signal = "LOW" | "HIGH";
 type ModuleType = "&" | "b" | "%";
-const modules = new Map<string, { type: ModuleType; destinations: string[] }>();
-
-raw.forEach((module) => {
-  const [prefix, destinations] = module.split(" -> ");
-  const [type, ...name] = prefix.split("");
-  modules.set(name.join(""), { type: type as ModuleType, destinations: destinations.split(", ") });
-});
+const modules = raw.toObject(
+  (module) => module.split(" -> ")[0].substring(1),
+  (module) => {
+    const [prefix, destinations] = module.split(" -> ");
+    return { type: prefix.charAt(0) as ModuleType, destinations: destinations.split(", ") };
+  }
+);
 
 export const func1 = () => {
-  let flipFlopStates = [...modules]
+  const flipFlopStates = Object.entries(modules)
     .filter((module) => module[1].type === "%")
     .toObject(
       ([name]) => name,
       () => false
     );
-  let conjunctionStates = [...modules]
+  const conjunctionStates = Object.entries(modules)
     .filter((module) => module[1].type === "&")
     .toObject(
       ([name]) => name,
       ([name]) =>
-        [...modules]
+        Object.entries(modules)
           .filter((module) => module[1].destinations.includes(name))
           .toObject(
             ([name]) => name,
-            () => "LOW" as Signal
+            () => "LOW"
           )
     );
 
-  console.log(flipFlopStates, conjunctionStates);
+  return _.range(0, 1000)
+    .reduce(
+      ([low, high]) => {
+        const queue = [{ from: "", destination: "roadcaster", signal: "LOW" }] as { from: string; destination: string; signal: string }[];
 
-  let high = 0;
-  let low = 0;
+        while (queue.length) {
+          const { from, destination, signal } = queue.shift()!;
+          const module = modules[destination];
+          if (!module) continue;
+          const { type, destinations } = module;
 
-  for (let i = 0; i < 1000; i++) {
-    low++;
-    let queue = [{ from: "", destination: "roadcaster", signal: "LOW" }] as { from: string; destination: string; signal: Signal }[];
-    while (queue.length) {
-      const { from, destination, signal } = queue.shift()!;
+          let signalToSend = "LOW";
 
-      const module = modules.get(destination);
+          if (type === "%") {
+            if (signal === "HIGH") continue;
+            flipFlopStates[destination] = !flipFlopStates[destination];
+            if (flipFlopStates[destination]) signalToSend = "HIGH";
+          }
 
-      if (!module) continue;
+          if (type === "&") {
+            conjunctionStates[destination][from] = signal;
+            if (Object.values(conjunctionStates[destination]).some((pulse) => pulse === "LOW")) signalToSend = "HIGH";
+          }
 
-      const { type, destinations } = module;
+          if (type === "b") signalToSend = signal;
 
-      if (type === "%" && signal !== "HIGH") {
-        flipFlopStates[destination] = !flipFlopStates[destination];
-        const signalToSend = flipFlopStates[destination] ? "HIGH" : ("LOW" as Signal);
-        if (signalToSend === "HIGH") {
-          high += destinations.length;
-        } else {
-          low += destinations.length;
+          signalToSend === "HIGH" ? (high += destinations.length) : (low += destinations.length);
+          queue.push(
+            ...destinations.map((module) => ({
+              from: destination,
+              destination: module,
+              signal: signalToSend,
+            }))
+          );
         }
-        queue.push(
-          ...destinations.map((module) => ({
-            from: destination,
-            destination: module,
-            signal: signalToSend,
-          }))
-        );
-        continue;
-      }
-
-      if (type === "&") {
-        conjunctionStates[destination][from] = signal;
-        const signalToSend = Object.values(conjunctionStates[destination]).some((pulse) => pulse === "LOW") ? "HIGH" : ("LOW" as Signal);
-        if (signalToSend === "HIGH") {
-          high += destinations.length;
-        } else {
-          low += destinations.length;
-        }
-        queue.push(
-          ...destinations.map((module) => ({
-            from: destination,
-            destination: module,
-            signal: signalToSend,
-          }))
-        );
-      }
-
-      if (type === "b") {
-        if (signal === "HIGH") {
-          high += destinations.length;
-        } else {
-          low += destinations.length;
-        }
-        queue.push(
-          ...destinations.map((module) => ({
-            from: destination,
-            destination: module,
-            signal,
-          }))
-        );
-      }
-    }
-  }
-
-  return high * low;
+        return [low, high];
+      },
+      [1000, 0]
+    )
+    .product();
 };
 
 const getLoop = (node: string) => {
-  let flipFlopStates = new Map<string, boolean>();
-  let conjunctionStates = new Map<string, Map<string, Signal>>();
-  [...modules].filter((module) => module[1].type === "%").forEach(([name]) => flipFlopStates.set(name, false));
-  [...modules]
+  const flipFlopStates = Object.entries(modules)
+    .filter((module) => module[1].type === "%")
+    .toObject(
+      ([name]) => name,
+      () => false
+    );
+  const conjunctionStates = Object.entries(modules)
     .filter((module) => module[1].type === "&")
-    .forEach(([name]) => {
-      const map = new Map<string, Signal>();
-      [...modules].filter((module) => module[1].destinations.includes(name)).forEach(([name]) => map.set(name, "LOW"));
-      conjunctionStates.set(name, map);
-    });
+    .toObject(
+      ([name]) => name,
+      ([name]) =>
+        Object.entries(modules)
+          .filter((module) => module[1].destinations.includes(name))
+          .toObject(
+            ([name]) => name,
+            () => "LOW"
+          )
+    );
 
   let buttonPresses = 0;
 
   while (true) {
     buttonPresses++;
-    const queue = [{ from: "", destination: "roadcaster", signal: "LOW" }] as { from: string; destination: string; signal: Signal }[];
+    const queue = [{ from: "", destination: "roadcaster", signal: "LOW" }] as { from: string; destination: string; signal: string }[];
 
     while (queue.length) {
       const { from, destination, signal } = queue.shift()!;
-
-      const module = modules.get(destination);
-
+      const module = modules[destination];
       if (!module) continue;
-
       const { type, destinations } = module;
 
-      if (type === "%" && signal !== "HIGH") {
-        const currState = flipFlopStates.get(destination);
-        flipFlopStates.set(destination, !currState);
-        const signalToSend = currState ? "LOW" : ("HIGH" as Signal);
+      let signalToSend = "LOW";
 
-        queue.push(
-          ...destinations.map((module) => ({
-            from: destination,
-            destination: module,
-            signal: signalToSend,
-          }))
-        );
-        continue;
+      if (type === "%") {
+        if (signal === "HIGH") continue;
+        flipFlopStates[destination] = !flipFlopStates[destination];
+        if (flipFlopStates[destination]) signalToSend = "HIGH";
       }
 
       if (type === "&") {
-        const conjunctionState = conjunctionStates.get(destination)!;
-        conjunctionState.set(from, signal);
-        const signalToSend = [...conjunctionState.values()].some((pulse) => pulse === "LOW") ? "HIGH" : ("LOW" as Signal);
-        if (signalToSend === "HIGH" && destination === node) return buttonPresses;
-
-        queue.push(
-          ...destinations.map((module) => ({
-            from: destination,
-            destination: module,
-            signal: signalToSend,
-          }))
-        );
-        continue;
+        conjunctionStates[destination][from] = signal;
+        if (Object.values(conjunctionStates[destination]).some((pulse) => pulse === "LOW")) signalToSend = "HIGH";
       }
 
-      if (type === "b") {
-        queue.push(
-          ...destinations.map((module) => ({
-            from: destination,
-            destination: module,
-            signal,
-          }))
-        );
-        continue;
-      }
+      if (type === "b") signalToSend = signal;
+
+      if (signalToSend === "HIGH" && destination === node) return buttonPresses;
+      queue.push(
+        ...destinations.map((module) => ({
+          from: destination,
+          destination: module,
+          signal: signalToSend,
+        }))
+      );
     }
   }
 };
 
-export const func2 = () => {
-  return [...modules].filter((module) => module[1].destinations.includes("kj")).lcm(([name]) => getLoop(name));
-};
+export const func2 = () =>
+  Object.entries(modules)
+    .filter((module) => module[1].destinations.includes("kj"))
+    .lcm(([name]) => getLoop(name));
