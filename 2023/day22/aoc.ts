7247d97cf3cfab22044e9ea0cf2fd83ce418770d
Balczer Dominik <balczer.dominik@gmail.com>
2023-12-23 12:46:38 +0100

    tidy up

--- 2023/day22/aoc.ts
+++ 2023/day22/aoc.ts
@@ -7,69 +7,56 @@ const [input, testInput] = readInputs(__dirname);
 const TEST = false;
 const raw = TEST ? testInput : input;
 
+type Brick = Triplet[];
+type SupportGraph = Map<string, string[]>;
+
 const bricks = raw
   .map((row) => {
-    const [start, end] = row.split("~");
-    const [sx, sy, sz] = start.numberSequence(",");
-    const [ex, ey, ez] = end.numberSequence(",");
-
+    const [sx, sy, sz, ex, ey, ez] = row.split("~").flatMap((coord) => coord.numberSequence(","));
     return _.range(sx, ex + 1).flatMap((x) => _.range(sy, ey + 1).flatMap((y) => _.range(sz, ez + 1).map((z) => [x, y, z])));
   })
   .sort((a, b) => a.max((c) => c[Z]) - b.max((c) => c[Z]));
 
-const dropBricks = () => {
-  const settledBricks: Triplet[][] = [];
-  const supportGraph = new Map<string, string[]>();
+const dropBricks = () =>
+  bricks.reduce(
+    ([settledBricks, supportGraph]: [Brick[], SupportGraph], fallingBrick) => {
+      const supportingBrickCandidates = settledBricks.filter((settledBrick) =>
+        settledBrick.some(([x, y]) => fallingBrick.some((c) => c[X] === x && c[Y] === y))
+      );
+      const highestZ = supportingBrickCandidates.max((brick) => brick.max((t) => t[Z])) ?? 0;
+      const supportingBricks = supportingBrickCandidates.filter((brick) => brick.max((t) => t[Z]) === highestZ);
 
-  bricks.forEach((fallingBrick) => {
-    const supportingBrickCandidates = settledBricks.filter((settledBrick) => {
-      return settledBrick.some(([x, y]) => fallingBrick.some((c) => c[X] === x && c[Y] === y));
-    });
-    const highestZ = supportingBrickCandidates.max((brick) => brick.max((t) => t[Z])) ?? 0;
-    const supportingBricks = supportingBrickCandidates.filter((brick) => brick.max((t) => t[Z]) === highestZ);
+      const zBase = fallingBrick.min((c) => c[Z]);
+      const settledBrick = fallingBrick.map(([x, y, z]) => [x, y, highestZ + (z - zBase) + 1]);
 
-    const zBase = fallingBrick.min((c) => c[Z]);
-    const settledCoords = fallingBrick.map(([x, y, z]) => [x, y, highestZ + (z - zBase) + 1]) as Triplet[];
+      supportingBricks.forEach((supportingBrick) => {
+        const stringified = JSON.stringify(supportingBrick);
+        const entry = supportGraph.get(stringified) ?? [];
+        supportGraph.set(stringified, [...entry, JSON.stringify(settledBrick)]);
+      });
 
-    supportingBricks.forEach((supportingBrick) => {
-      const stringified = JSON.stringify(supportingBrick);
-      const entry = supportGraph.get(stringified) ?? [];
-      supportGraph.set(stringified, [...entry, JSON.stringify(settledCoords)]);
-    });
+      return [[...settledBricks, settledBrick], supportGraph] as [Brick[], SupportGraph];
+    },
+    [[], new Map()]
+  );
 
-    settledBricks.push(settledCoords);
-  });
-
-  return [settledBricks, supportGraph] as const;
-};
-
-const getNonCriticalBricks = (settledBricks: Triplet[][], supportGraph: Map<string, string[]>) => {
-  return settledBricks.filter((brick) => {
+const getNonCriticalBricks = (settledBricks: Brick[], supportGraph: SupportGraph) =>
+  settledBricks.filter((brick) => {
     const supporting = supportGraph.get(JSON.stringify(brick));
-    if (!supporting) return true;
-
-    return supporting.every((brick) => {
-      const supportedBy = [...supportGraph.values()].filter((s) => s.includes(brick)).length;
-      return supportedBy >= 2;
-    });
+    return !supporting || supporting.every((brick) => [...supportGraph.values()].filter((s) => s.includes(brick)).length >= 2);
   });
-};
 
-export const func1 = () => {
-  const [settledBricks, supportGraph] = dropBricks();
-
-  return getNonCriticalBricks(settledBricks, supportGraph).length;
-};
+export const func1 = () => getNonCriticalBricks(...dropBricks()).length;
 
 export const func2 = () => {
   const [settledBricks, supportGraph] = dropBricks();
   const nonCriticalBricks = getNonCriticalBricks(settledBricks, supportGraph);
-  const reverseSupportGraph = new Map<string, string[]>(); //shows which blocks support each block
+  const reverseSupportGraph: SupportGraph = new Map();
 
   settledBricks.forEach((brick) => {
     if (brick.some((c) => c[Z] === 1)) return;
     const stringified = JSON.stringify(brick);
-    const supportedBy = [...supportGraph.entries()].filter((b) => b[1].includes(stringified)).map((b) => b[0]);
+    const supportedBy = [...supportGraph.entries()].filter((brick) => brick[1].includes(stringified)).map((brick) => brick[0]);
     reverseSupportGraph.set(stringified, supportedBy);
   });
 
@@ -83,17 +70,14 @@ export const func2 = () => {
       while (queue.length) {
         const toRemove = queue.shift()!;
         for (const copyKey in copy) {
-          const newSupporting = copy[copyKey].filter((b) => b !== toRemove);
-          copy[copyKey] = newSupporting;
-          if (!newSupporting.length) {
+          copy[copyKey] = copy[copyKey].filter((brick) => brick !== toRemove);
+          if (!copy[copyKey].length) {
             queue.push(copyKey);
             delete copy[copyKey];
             removed++;
           }
         }
       }
-
-      console.log(brick, removed);
 
       return removed;
     });
