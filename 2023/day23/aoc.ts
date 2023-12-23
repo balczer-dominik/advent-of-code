55db8f34218aa539bbba4fd87d921651212709d4
Balczer Dominik <balczer.dominik@gmail.com>
2023-12-23 18:58:43 +0100

    day 23 done

---
+++ 2023/day23/aoc.ts
@@ -0,0 +1,98 @@
+import { readInputs } from "../../util/input";
+import "../../util/helpers";
+import { DOWN, Direction2DOrthogonal, RIGHT, directions2DOrthogonal, move2D } from "../../util/2d";
+import _ from "lodash";
+
+const [input, testInput] = readInputs(__dirname);
+const TEST = false;
+const raw = TEST ? testInput : input;
+const neighborMap = new Map<string, string[]>();
+const neighborMapButILearnedToClimb = new Map<string, string[]>();
+
+type Slope = ">" | "v";
+type Trail = { head: string; visited: Set<string> };
+const slopToDir: Record<Slope, Direction2DOrthogonal> = {
+  ">": RIGHT,
+  v: DOWN,
+};
+
+raw.forEach((row, y) =>
+  row.split("").forEach((c, x) => {
+    if (c === "#") return;
+    const neighbors = directions2DOrthogonal
+      .filter((dir) => c === "." || dir === slopToDir[c as Slope])
+      .map((dir) => move2D([x, y], dir))
+      .filter(([x, y]) => [".", "v", ">"].includes(raw.at(y)?.at(x) ?? ""))
+      .map(([x, y]) => `${x}=${y}`);
+    neighborMap.set(`${x}=${y}`, neighbors);
+  })
+);
+
+raw.forEach((row, y) =>
+  row.split("").forEach((c, x) => {
+    if (c === "#") return;
+    const neighbors = directions2DOrthogonal
+      .map((dir) => move2D([x, y], dir))
+      .filter(([x, y]) => [".", "v", ">"].includes(raw.at(y)?.at(x) ?? ""))
+      .map(([x, y]) => `${x}=${y}`);
+    neighborMapButILearnedToClimb.set(`${x}=${y}`, neighbors);
+  })
+);
+
+const getMostScenicTrail = (neighborMap: Map<string, string[]>) => {
+  const junctions = raw.flatMap((row, y) =>
+    row.split("").flatMap((c, x) => (c === "#" || (neighborMap.get(`${x}=${y}`)?.length ?? 0) <= 2 ? [] : [`${x}=${y}`]))
+  );
+
+  junctions.push(`${1}=${0}`, `${raw[0].length - 2}=${raw.length - 1}`);
+
+  const junctionNeighbors = new Map<string, { [key: string]: number }>();
+
+  junctions.forEach((junction) => {
+    const queue: Trail[] = [{ head: junction, visited: new Set() }];
+    queue[0].visited.add(junction);
+    const neighbors: { [key: string]: number } = {};
+
+    while (queue.length) {
+      const { head, visited } = queue.shift()!;
+
+      neighborMap
+        .get(head)!
+        .filter((node) => !visited.has(node))
+        .forEach((newHead) => {
+          if (junctions.includes(newHead)) {
+            neighbors[newHead] = visited.size;
+            return;
+          }
+
+          const newVisited = _.cloneDeep(visited);
+          newVisited.add(newHead);
+          queue.push({ head: newHead, visited: newVisited });
+        });
+    }
+
+    junctionNeighbors.set(junction, neighbors);
+  });
+
+  const queue: string[][] = [[`${1}=${0}`]];
+
+  let longest = 0;
+
+  while (queue.length) {
+    const visited = queue.pop()!;
+
+    if (visited.last()! === `${raw[0].length - 2}=${raw.length - 1}`) {
+      const distance = _.range(0, visited.length - 1).sum((idx) => junctionNeighbors.get(visited[idx])![visited[idx + 1]]);
+      if (longest < distance) longest = distance;
+    }
+
+    Object.keys(junctionNeighbors.get(visited.last()!)!)
+      .filter((junction) => !visited.includes(junction))
+      .forEach((nextJunction) => queue.push([...visited, nextJunction]));
+  }
+
+  return longest;
+};
+
+export const func1 = () => getMostScenicTrail(neighborMap);
+export const func2 = () => getMostScenicTrail(neighborMapButILearnedToClimb);
